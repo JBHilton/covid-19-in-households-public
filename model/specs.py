c6c4f38c6812bbdc404b3fd6188c744144c57884
JBHilton <j.hilton@warwick.ac.uk>
2020-05-25 09:30:06 +0100

    Added variable descriptions in specs.oy

--- model/specs.py
+++ model/specs.py
@@ -3,9 +3,9 @@
 DEFAULT_SPEC = {
     # Interpretable parameters:
     'R0': 2.4,                      # Reproduction number
-    'gamma': 0.5,                   # Some scaling parameter?
-    'alpha': 0.2,                   # Some scaling parameter?
-    'tau': 0.0,                     # Rate of asymptomatic transmission?
+    'gamma': 0.5,                   # Mean infectious period
+    'alpha': 0.2,                   # Incubation period
+    'tau': 0.0,                     # Asymptomatic transmission intensity relative to symptomatic rate
     'det_model': {
         'type': 'scaled',           # 'constant' and 'scaled' are the two options
         'max_det_fraction': 0.9     # Cap for detected cases (90%)
