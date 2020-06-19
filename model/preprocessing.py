af2fca0d509f3c4664106da0698c4b3e9bfbe005
JBHilton <j.hilton@warwick.ac.uk>
2020-06-19 09:13:18 +0100

    Moved excel and cvs reads to specs file
    All of the read_excel and read_csv operations from model.preprocessing are now done in model.specs. Moving the read operations in this way should make it easier to adapt the code to other population settings. Moving forward, it might make sense to store each national setting in a separate dictionary in model.specs.

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -201,39 +201,39 @@ class ModelInput:
     '''TODO: add docstring'''
     def __init__(self, spec):
         self.spec = deepcopy(spec)
-        k_home = read_excel(
-            'inputs/MUestimates_home_2.xlsx',
-            sheet_name='United Kingdom of Great Britain',
-            header=None).to_numpy()
-        k_all = read_excel(
-            'inputs/MUestimates_all_locations_2.xlsx',
-            sheet_name='United Kingdom of Great Britain',
-            header=None).to_numpy()
+        # k_home = read_excel(
+        #     'inputs/MUestimates_home_2.xlsx',
+        #     sheet_name='United Kingdom of Great Britain',
+        #     header=None).to_numpy()
+        # k_all = read_excel(
+        #     'inputs/MUestimates_all_locations_2.xlsx',
+        #     sheet_name='United Kingdom of Great Britain',
+        #     header=None).to_numpy()
 
         # Because we want 80 to be included as well.
         fine_bds = arange(0, 81, 5)
         self.coarse_bds = concatenate((fine_bds[:6], fine_bds[12:]))
 
-        pop_pyramid = read_csv(
-            'inputs/United Kingdom-2019.csv', index_col=0)
-        pop_pyramid = (pop_pyramid['F'] + pop_pyramid['M']).to_numpy()
+        # pop_pyramid = read_csv(
+        #     'inputs/United Kingdom-2019.csv', index_col=0)
+        pop_pyramid = (spec['pop_pyramid']['F'] + spec['pop_pyramid']['M']).to_numpy()
 
         self.k_home = aggregate_contact_matrix(
-            k_home, fine_bds, self.coarse_bds, pop_pyramid)
+            spec['k_home'], fine_bds, self.coarse_bds, pop_pyramid)
         self.k_all = aggregate_contact_matrix(
-            k_all, fine_bds, self.coarse_bds, pop_pyramid)
+            spec['k_all'], fine_bds, self.coarse_bds, pop_pyramid)
         self.k_ext = self.k_all - self.k_home
 
         # This is in ten year blocks
-        rho = read_csv(
-            'inputs/rho_estimate_cdc.csv', header=None).to_numpy().flatten()
+        # rho = read_csv(
+        #     'inputs/rho_estimate_cdc.csv', header=None).to_numpy().flatten()
 
         cdc_bds = arange(0, 81, 10)
         aggregator = make_aggregator(cdc_bds, fine_bds)
 
         # This is in five year blocks
         rho = sparse((
-            rho[aggregator],
+            spec['rho'][aggregator],
             (arange(len(aggregator)),[0]*len(aggregator))))
 
         rho = spec['gamma'] * spec['R0'] * aggregate_vector_quantities(
--- model/specs.py
+++ model/specs.py
@@ -1,5 +1,7 @@
 '''Module containing model specifications'''
 
+from pandas import read_excel, read_csv
+
 DEFAULT_SPEC = {
     # Interpretable parameters:
     'R0': 2.4,                      # Reproduction number
@@ -9,6 +11,17 @@ DEFAULT_SPEC = {
     'det_model': {
         'type': 'scaled',           # 'constant' and 'scaled' are the two options
         'max_det_fraction': 0.9     # Cap for detected cases (90%)
-    }
-    # TODO: Consider adding input files here: xlsx, csv etc.
+    },
+    'k_home': read_excel(
+        'inputs/MUestimates_home_2.xlsx',
+        sheet_name='United Kingdom of Great Britain',
+        header=None).to_numpy(),
+    'k_all': read_excel(
+        'inputs/MUestimates_all_locations_2.xlsx',
+        sheet_name='United Kingdom of Great Britain',
+        header=None).to_numpy(),
+    'pop_pyramid': read_csv(
+        'inputs/United Kingdom-2019.csv', index_col=0),
+    'rho': read_csv(
+        'inputs/rho_estimate_cdc.csv', header=None).to_numpy().flatten()
 }
