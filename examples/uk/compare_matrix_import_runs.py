55e785d3102850925d98b3375f60c8df38707ebf
Joe Hilton <joe.b.hilton@gmail.com>
2025-02-07 17:34:53 +0000

    Updated error function in compare_matrix_import_runs.py
    Rather than calculating error over all elements where probability is above 10^-9, we now calculate error over bottom 99.9% of state distribution at each time point.

--- examples/uk/compare_matrix_import_runs.py
+++ examples/uk/compare_matrix_import_runs.py
@@ -1,7 +1,7 @@
 '''This runs the UK-like model with a single set of parameters for 100 days
 '''
 from copy import deepcopy
-from numpy import arange, array, exp, log, where
+from numpy import arange, array, asarray, cumsum, exp, log, where
 from os import mkdir
 from os.path import isdir, isfile
 from pickle import load, dump
@@ -10,7 +10,7 @@ from scipy.integrate import solve_ivp
 from model.preprocessing import ( estimate_beta_ext, estimate_growth_rate,
         SEPIRInput, HouseholdPopulation, make_initial_condition_by_eigenvector)
 from model.specs import TWO_AGE_SEPIR_SPEC_FOR_FITTING, TWO_AGE_UK_SPEC
-from model.common import SEPIRRateEquations, MatrixImportSEPIRRateEquations, UnloopedSEPIRRateEquations, LogSEPIRRateEquations
+from model.common import SEPIRRateEquations, MatrixImportSEPIRRateEquations, UnloopedSEPIRRateEquations
 from model.imports import FixedImportModel, NoImportModel, ExponentialImportModel
 
 # pylint: disable=invalid-name
@@ -73,7 +73,6 @@ rhs = SEPIRRateEquations(model_input, household_population, fixed_imports)
 rhs.epsilon = 0
 rhs_M = MatrixImportSEPIRRateEquations(model_input, household_population, exp_imports)
 rhs_U = UnloopedSEPIRRateEquations(model_input, household_population, fixed_imports, sources="IMPORT")
-rhs_L = LogSEPIRRateEquations(model_input, household_population, fixed_imports, sources="IMPORT")
 
 H0 = make_initial_condition_by_eigenvector(growth_rate, model_input, household_population, rhs, 0., 0.0,False,3)
 S0 = H0.T.dot(household_population.states[:, ::5])
@@ -87,10 +86,6 @@ start_state = (1/model_input.ave_hh_size) * array([S0.sum(),
                                                    I0.sum(),
                                                    R0.sum()])
 
-# Initial conditions for log model
-Z0 = log(H0)
-Z0[where(H0==0)[0]] = -100
-
 T_end = 365.
 tspan = (0.0, T_end)
 import time
@@ -103,9 +98,6 @@ print("Non-matrix took", time.time()- nm_start)
 u_start = time.time()
 solution_U = solve_ivp(rhs_U, tspan, H0, first_step=0.001, atol=1e-16, t_eval = arange(0., T_end, 1.))
 print("U took", time.time() - u_start)
-l_start = time.time()
-solution_L = solve_ivp(rhs_L, tspan, Z0, first_step=0.001, atol=1e-16, t_eval = arange(0., T_end, 1.))
-print("L took", time.time() - l_start)
 
 H = solution.y
 R = H.T.dot(household_population.states[:, 4::5])
@@ -115,13 +107,27 @@ R = H.T.dot(household_population.states[:, 4::5])
 H_U = solution_U.y
 R_U = H_U.T.dot(household_population.states[:, 4::5])
 
-Z_L = solution_L.y
-H_L = exp(Z_L)
-R_L = H_L.T.dot(household_population.states[:, 4::5])
+# Get bottom 99.9% of state distribution at each time step for error calculation. This should be a reasonably
+# systematic way to avoid getting big errors from taking ratios between very small probabilities.
+desc_order = (-H).argsort(axis=0)
+H_desc = H[desc_order]
+H_cumsum = 0 * H
+H_cumsum[desc_order] = cumsum(H_desc, axis=0) # This gives cumulative sum starting from largest to smallest element without reordering entries
+locs_for_err = [where(H_cumsum[:, t]<.999) for t in range(H.shape[1])] # For each time step return a list of locations with cumulative sum below 99.9%
+
+daily_max_err = asarray([
+    max((abs((H[locs_for_err[t], t]-H_U[locs_for_err[t], t]))/H[locs_for_err[t], t]))[0] for t in range(H.shape[1])])
+print("Max relative error in H_U along bottom 99.99% of distribution is", daily_max_err.max())
+
+# For comparison with static cutoff, check what cutoff we are effectively using here:
+leaveout_locs = [where(H_cumsum[:, t]>=.999) for t in range(H.shape[1])]
+effective_cutoff = asarray([
+    max(H[leaveout_locs[t], t])[0] for t in range(H.shape[1])])
+print("Max effective cutoff:", effective_cutoff.max())
+print("Mean effective cutoff:", effective_cutoff.mean())
 
 # Print disagreement with cutoff of 1e-9 so that we don't return high errors when
 # both are very close to zero, i.e. if H_M~1e-n, H~1e-m for large n and m we don't
 # want the relative error to be 1e-(n-m).
 # print("Max relative error in H_M for H>1e-9 is", max(abs((H-H_M))[H>1e-9]/H[H>1e-9]))
 print("Max relative error in H_U for H>1e-9 is", max(abs((H-H_U))[H>1e-9]/H[H>1e-9]))
-print("Max relative error in H_L for H>1e-9 is", max(abs((H_U-H_L))[H_U>1e-9]/H_U[H_U>1e-9]))
