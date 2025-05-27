70dd0ed0375453c2a5e37faf10d94c31d0cfd044
igmsb18 <154246525+igmsb18@users.noreply.github.com>
2025-05-27 16:25:42 +0100

    Replaced the llh
    But there is a division by zero in log and aparently the state errror contains NaNs at the first time point

--- examples/inference-from-testing/simulation_inference.py
+++ examples/inference-from-testing/simulation_inference.py
@@ -25,7 +25,7 @@ os.getcwd()
 
 # Import functions from the project code
 from copy import deepcopy
-from numpy import arange, array, atleast_2d, log
+from numpy import arange, array, atleast_2d, log, where
 from os import mkdir
 from os.path import isdir, isfile
 from pickle import load, dump
@@ -135,181 +135,83 @@ def run_simulation(lambda_val, tau_val):
     # Generate test data:
 
     multi_hh_data = [generate_single_hh_test_data(test_times) for i in range(n_hh)]
-    return multi_hh_data
+    return multi_hh_data, base_rhs
 
+def one_step_household_llh(hh_data,
+                  test_times,
+                  tau,
+                  lam,
+                  base_rhs,
+                 growth_rate,
+                 init_prev,
+                 R_comp):
+    '''This function calculates the log likelihood of parameters tau and lam given some data with test results at two
+    time points for a single household. This assumes data comes as number of positive tests at start and end time point,
+    and that each positive test corresponds to exactly one individual in the I compartment of the model.'''
+    rhs = deepcopy(base_rhs)
+    rhs.int_rate *= tau
+    rhs.ext_rate *= lam
 
-# Function to compute log-likelihood from test data
-def llh_from_test_data(test_data, test_ts, rhs, H0):
-    total_sol_time = 0
-    Ht = deepcopy(H0)
+    H0 = make_initial_condition_by_eigenvector(growth_rate,
+                                               rhs.model_input,
+                                               rhs.household_population,
+                                               rhs,
+                                               init_prev,
+                                               0.0,
+                                               False,
+                                               R_comp)
+    Ht = 0 * H0 # Set up initial condition vector
+    possible_states = where(abs(sum(rhs.states_inf_only, 1) - hh_data[0]) < 1e-1)[0]
+    # Set Ht equal to eigenvector initial condition, conditioned on test results
+    Ht[possible_states, ] = H0[possible_states, ] / sum(H0[possible_states, ])
     llh = 0
-    for i in range(len(test_times)-1):
-        if i==0:
-            start_time = 0
-        else:
-            start_time = test_times[i-1]
-        tspan = (start_time, test_times[i])
-        pre_sol = time.time()
-        solution = solve_ivp(rhs, tspan, Ht, first_step=0.001, atol=1e-16)
-        total_sol_time += time.time()-pre_sol
-        T = solution.t
-        H = solution.y
-        I = test_data[i]
-        possible_states = np.where(np.abs(np.sum(rhs.states_inf_only,1)-I)<1e-1)[0]
-        llh += np.log(np.sum(H[possible_states, -1]))
-        Ht *= 0
-        Ht[possible_states] = H[possible_states, -1]
-    #print(total_sol_time)
+
+    tspan = (test_times[0], test_times[1])
+    solution = solve_ivp(rhs, tspan, Ht, first_step=0.001, atol=1e-16)
+    T = solution.t
+    H = solution.y
+    I = hh_data[1]
+    possible_states = where(abs(sum(rhs.states_inf_only,1)-I)<1e-1)[0]
+    llh += log(sum(H[possible_states, -1]))
     return(llh)
 
-
-# Function to calculate log-likelihood from parameter values
-def llh_from_pars(data, test_times, tau, lam):
-    pre_hh_time = time.time()
-    model_input = SEIRInput(SPEC, composition_list, comp_dist, print_ests=False)
-    model_input.k_home *= tau / model_input_to_fit.beta_int
-    model_input.beta_int = tau
-    model_input.k_ext *= lam
-
-    true_density_expo = model_input.density_expo
-    # With the parameters chosen, we calculate Q_int:
-    household_population = HouseholdPopulation(
-        composition_list, comp_dist, model_input)
-    # lam goes in as arg for rate equations
-
-    no_imports = NoImportModel(4, 1)
-
-    base_rhs = UnloopedSEIRRateEquations(model_input, household_population, no_imports)
-    base_H0 = make_initial_condition_by_eigenvector(growth_rate,
-                                                    model_input,
-                                                    household_population,
-                                                    base_rhs,
-                                                    1e-1,
-                                                    0.0,
-                                                    False,
-                                                    3)
-    x0 = base_H0.T.dot(household_population.states[:, 1::4])
-
-    fixed_imports = FixedImportModel(4,
-                                     1,
-                                     base_rhs,
-                                     x0)
-    rhs = UnloopedSEIRRateEquations(model_input, household_population, fixed_imports)
-    H0 = make_initial_condition_by_eigenvector(growth_rate, model_input, household_population, rhs, 1e-2, 0.0,
-                                               False, 3)
-    tspan = (0.0, 365)
-    # print(time.time() - pre_hh_time)
-    return (sum(array([llh_from_test_data(data_i, test_times, rhs, H0) for data_i in data])))
-
+def one_step_population_likelihood(test_data,
+                  test_times,
+                  tau,
+                  lam,
+                  base_rhs,
+                 growth_rate,
+                 init_prev,
+                 R_comp):
+    '''This is a wrapper for one_step_household_llh function, which allows for the calculation of the joint likelihood
+    of an entire population's worth of independent one-step testing samples in a single function call.'''
+    return (sum(array([one_step_household_llh(data_i,
+                  test_times,
+                  tau,
+                  lam,
+                  base_rhs,
+                 growth_rate,
+                 init_prev,
+                 R_comp) for data_i in test_data])))
 
 # Argument to type in console to run run_inference
-multi_hh_data = run_simulation(3.0, .09)
+#multi_hh_data = run_simulation(3.0, .09)
+multi_hh_data, base_rhs = run_simulation(3.0, .09)
 
 
 # Run inference to estimate tau and lambda
-def run_inference(multi_hh_data):
+def run_inference(multi_hh_data, base_rhs):
 
     def f(params):
         tau = params[0]
         lam = params[1]
-        return -llh_from_pars(multi_hh_data, test_times, tau, lam)
+        return -one_step_population_likelihood(multi_hh_data, test_times, tau, lam, base_rhs, growth_rate, init_prev=1e-2, R_comp=3)
 
     mle = sp.optimize.minimize(f, [tau_0, lambda_0], bounds=((0.0, 0.15), (2., 5.)))
     tau_est, lambda_est = mle.x[0], mle.x[1]
     return tau_est, lambda_est
 
 # Run run_inference
-run_inference(multi_hh_data)
-
-###############################
-#Plots
-###############################
-
-#1D Plot
-
-# Define parameter ranges
-tau_vals = np.linspace(0.05, 0.15, 100)
-lam_vals = np.linspace(2.5, 3.5, 100)
-
-# Assume sampled_households is multi_hh_data from earlier
-sampled_households = multi_hh_data
-true_lam = 3.0  # used in run_simulation
-true_tau = tau_0  # which is 0.09
-
-# Compute log-likelihood with fixed lambda
-llh_fixed_lam = np.zeros(len(tau_vals))
-for i in tqdm(range(len(tau_vals)), desc="Computing llh vs tau"):
-    llh_fixed_lam[i] = llh_from_pars(sampled_households, test_times, tau_vals[i], true_lam)
-
-# Compute log-likelihood with fixed tau
-llh_fixed_tau = np.zeros(len(lam_vals))
-for i in tqdm(range(len(lam_vals)), desc="Computing llh vs lambda"):
-    llh_fixed_tau[i] = llh_from_pars(sampled_households, test_times, true_tau, lam_vals[i])
-
-# Run inference to get estimated parameters
-tau_hat, lam_hat = run_inference(multi_hh_data)
-
-# Plot
-fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-
-ax1.plot(tau_vals, llh_fixed_lam, label='Log likelihood vs tau')
-ax1.set_xlabel("tau")
-ax1.set_ylabel("log likelihood")
-ax1.axvline(true_tau, color='k', linestyle='--', label='true value')
-ax1.axvline(tau_hat, color='r', linestyle='--', label='estimated value')
-ax1.legend(loc='lower right')
-
-ax2.plot(lam_vals, llh_fixed_tau, label='Log likelihood vs lambda')
-ax2.set_xlabel("lambda")
-ax2.set_ylabel("log likelihood")
-ax2.axvline(true_lam, color='k', linestyle='--', label='true value')
-ax2.axvline(lam_hat, color='r', linestyle='--', label='estimated value')
-ax2.legend(loc='lower right')
-
-plt.tight_layout()
-plt.savefig('outputs/1D llh')
-plt.close(fig)
-
-#2D Plot (heat map)
-
-# Define the tau and lambda ranges
-tau_vals = np.arange(0.060, 0.105, 0.01)
-lam_vals = np.arange(2., 4.0, 0.25)
-
-# Initialize a matrix to store log-likelihoods
-llh_vals = np.zeros((len(tau_vals), len(lam_vals)))
-
-# Compute the log-likelihoods
-for i in range(len(tau_vals)):
-    for j in range(len(lam_vals)):
-        llh_vals[i, j] = llh_from_pars(multi_hh_data, test_times, tau_vals[i], lam_vals[j])
-        print("tau =", tau_vals[i], ", lam =", lam_vals[j], ", llh[tau, lam] =", llh_vals[i, j])
-
-# Save the grid of results to a pickle file
-with open('outputs/inference-from-testing/synth-data-gridded-par-ests.pkl', 'wb') as f:
-    dump((tau_vals, lam_vals, llh_vals), f)
-
-# Create the plot
-fig, ax = plt.subplots(1, 1)
-
-lam_inc = lam_vals[1] - lam_vals[0]
-lam_max = lam_vals[-1] + lam_inc
-tau_inc = tau_vals[1] - tau_vals[0]
-tau_max = tau_vals[-1] + tau_inc
-
-# Plot heatmap of log-likelihoods
-im = ax.imshow(llh_vals, origin='lower',
-               extent=(lam_vals[0] - 0.5 * lam_inc, lam_max - 0.5 * lam_inc,
-                       tau_vals[0] - 0.5 * tau_inc, tau_max - 0.5 * tau_inc),
-               aspect=(lam_max - lam_vals[0]) / (tau_max - tau_vals[0]))
-
-# Add labels and marker for true parameter values
-ax.set_xlabel("lambda")
-ax.set_ylabel("tau")
-ax.plot([3.0], [0.09], marker=".", ms=20, label="True value", color='red')
-ax.legend()
-
-# Save the figure
-plt.savefig('outputs/2D llh')
-plt.close(fig)
+#run_inference(multi_hh_data)
+run_inference(multi_hh_data, base_rhs)
 
